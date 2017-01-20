3f54b72a2e2d26ab4ae6a4dfba5eddf8bd57b279
rachel-rocks-at-ruby <rachelb@gmail.com>
2017-01-19 19:18:40 -0800

    made wider card for About

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,19 +5,23 @@ export default () => {
     <main className="mdl-layout__content">
       <div className="page-content mdl-cell mdl-cell--6-col">
         <h1>About</h1>
-        <p>Working Women Advocates are here to help you through difficulties at work including, but not limited to:</p>
+        <div className="card-wide mdl-card mdl-shadow--2dp">
+          <div className="mdl-card__supporting-text">
+            <p>Working Women Advocates are here to help you through difficulties at work including, but not limited to:</p>
 
-        <ul className='mdl-list'>
-          <li className="mdl-list__item">Harassment</li>
-          <li className="mdl-list__item">Sexism</li>
-          <li className="mdl-list__item">Exclusion</li>
-          <li className="mdl-list__item">Discrimination</li>
-          <li className="mdl-list__item">Assault</li>
-        </ul>
+            <ul className='mdl-list'>
+              <li className="mdl-list__item">Harassment</li>
+              <li className="mdl-list__item">Sexism</li>
+              <li className="mdl-list__item">Exclusion</li>
+              <li className="mdl-list__item">Discrimination</li>
+              <li className="mdl-list__item">Assault</li>
+            </ul>
 
-        <p>When you experience these types of problems, it can be difficult to find guidance to navigate your way throughd. Working Women Advocates are trained to provide emotional support, as well as come up with a plan to best handle your particular situation. They will be there to provide support as you execute the plan, and be available until you are well into your healing process.</p>
+            <p>When you experience these types of problems, it can be difficult to find guidance to navigate your way through. Working Women Advocates are trained to provide emotional support, as well as come up with a plan to best handle your particular situation. They will be there with you as you execute the plan, and available until you are well into your healing process.</p>
 
-        <p>Your privacy and security are of utmost important. You will never be asked for identifying information.</p>
+            <p>Your privacy and security are of utmost important. You will never be asked for identifying information.</p>
+          </div>
+        </div>
       </div>
     </main>
   )
