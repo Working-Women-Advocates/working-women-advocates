05d81f92e5bbf7fe19fdaa7c9a6373e19fc22194
rachel-rocks-at-ruby <rachelb@gmail.com>
2017-01-19 20:04:24 -0800

    fixed drawer and footer

--- app/components/Drawer.jsx
+++ app/components/Drawer.jsx
@@ -7,8 +7,9 @@ export default () => {
     <div className="mdl-layout__drawer">
       <span className="mdl-layout-title">WWA</span>
       <nav className="mdl-navigation">
-        <Link to="about"  className="mdl-navigation__link">About</Link>
-        <Link to="volunteer"  className="mdl-navigation__link">Volunteer</Link>
+        <Link to="landing" className="mdl-navigation__link">Home</Link>
+        <Link to="about" className="mdl-navigation__link">About</Link>
+        <Link to="volunteer" className="mdl-navigation__link">Volunteer</Link>
         <a className="mdl-navigation__link" href>Mentor Login</a>
         <a className="mdl-navigation__link" href>Working Women Login</a>
       </nav>
--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,10 +4,10 @@ export default () => {
   return (
     <footer className="mdl-mini-footer">
       <div className="mdl-mini-footer__left-section">
-        <div className="mdl-logo">Working Women Advocates</div>
+        <div className="mdl-logo">Made by <a href="http://rachelbird.com/" target="_blank" rel="noopener noreferrer">Rachel Bird</a></div>
         <ul className="mdl-mini-footer__link-list">
-          <li><a href="#">Help</a></li>
-          <li><a href="#">Privacy &amp; Terms</a></li>
+          <li><a href="https://www.fullstackacademy.com/" target="_blank" rel="noopener noreferrer">Project for Fullstack Academy</a></li>
+          <li><a href="http://www.forwomenintech.com/" target="_blank" rel="noopener noreferrer">For Women in Tech</a></li>
         </ul>
       </div>
     </footer>
