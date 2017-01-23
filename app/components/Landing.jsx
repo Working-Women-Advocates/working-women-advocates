117eab90ccd787882c4891ffa6743ee547d281db
rachel-rocks-at-ruby <rachelb@gmail.com>
2017-01-23 09:33:42 -0800

    typo

--- app/components/Landing.jsx
+++ app/components/Landing.jsx
@@ -6,7 +6,7 @@ export default () => {
     <main className="mdl-layout__content">
       <div id="landing-page" className="page-content">
         <h1>Working Women Advocates</h1>
-        <h6 className="mdl-cell mdl-cell--6-col">If you are experiencing difficulties at work, you may need an advocate. We are here for you and respct your privacy.</h6>
+        <h6 className="mdl-cell mdl-cell--6-col">If you are experiencing difficulties at work, you may need an advocate. We are here for you and respect your privacy.</h6>
         <div className="get-started">
           <Link to="working-women-signup" className="get-started-btn mdl-button mdl-js-button mdl-button--raised mdl-button--accent">Get Started</Link>
         </div>
