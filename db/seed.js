25fd130087f48ef58b127b1ac1571a090654f3e3
rachel-rocks-at-ruby <rachelb@gmail.com>
2017-02-15 13:17:38 -0800

    seeding

--- db/seed.js
+++ db/seed.js
@@ -9,8 +9,8 @@ const seedUsers = () => db.Promise.map([
 
 const seedIssues = () => db.Promise.map([
   {username: 'ChelseaManning', contact_method: 'Email', email: 'chelsea@example.com', description: 'sexism and exclusion', advocate_id: 2},
-  {username: 'SuperGirl', contact_method: 'Skype', skype: 'supergirl', description: 'sexual harrasment'},
-  {username: 'Lois Lane', contact_method: 'SMS Text', text: '555-555-5554', description: 'discrimination'},
+  {username: 'SuperGirl', contact_method: 'Skype', skype: 'supergirl', description: 'sexual harrasment', advocate_id: 1},
+  {username: 'Lois Lane', contact_method: 'SMS Text', text: '555-555-5554', description: 'discrimination', advocate_id: 1},
   {username: 'Beyonce', contact_method: 'Other', other: 'whatsapp', description: 'sexism', advocate_id: 2},
   {username: 'Taylor', contact_method: 'Phone', best_time: 'weekday evenings Pacific', phone: '555-555-5555', description: 'harassment'},
   {username: 'Kesha', contact_method: 'Other', other: 'viber', description: 'assault', status: 'closed'}
