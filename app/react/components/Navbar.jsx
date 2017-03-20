d09983d1cfd47154a0e2d00356d0368faf6a614a
Beth Qiang <bethany.qiang@gmail.com>
2017-03-19 20:31:21 -0500

    Move navbar into its own component

---
+++ app/react/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Navbar = () => (
+  <nav id="tf-menu" className="navbar navbar-default navbar-fixed-top">
+    <div className="container">
+      <div className="navbar-header">
+        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span className="sr-only">Toggle navigation</span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+        </button>
+        <a className="navbar-brand" href="index.html"><img style={{ 'marginTop': '-5px' }} src="img/wwalogo.png" /></a>
+      </div>
+      <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul className="nav navbar-nav navbar-right">
+          <li><a href="#tf-home" className="page-scroll">Home</a></li>
+          <li><a href="#tf-about" className="page-scroll">About</a></li>
+          <li><a href="#tf-team" className="page-scroll">Team</a></li>
+          <li><a href="#tf-services" className="page-scroll">Advocacy</a></li>
+          <li><a href="#tf-testimonials" className="page-scroll">Get Involved</a></li>
+          <li><a href="#tf-contact" className="page-scroll">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+)
+
+export default Navbar
