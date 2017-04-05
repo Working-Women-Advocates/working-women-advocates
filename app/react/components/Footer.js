9214af2d85ffe0e711a42f4d816cb934a080a3c9
Mark Hario <hario.mark@gmail.com>
2017-04-04 20:23:58 -0600

    removed extra imgs, removed inline styling from footer

--- app/react/components/Footer.js
+++ app/react/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="container">
         <div className="pull-left fnav">
           <p>ALL RIGHTS RESERVED. COPYRIGHT © 2017.</p>
-          <p style={{ 'fontSize': '10px', 'color': '#9E9E9E' }}>Template designed by <a href="https://dribbble.com/shots/1817781--FREEBIE-Spirit8-Digital-agency-one-page-template" target="blank" rel="noopener noreferrer" style={{ 'color': '#9E9E9E' }}>Robert Berki</a> and coded by <a href="https://dribbble.com/jennpereira" target="blank" rel="noopener noreferrer" style={{ 'color': '#9E9E9E' }}>Jenn Pereira</a></p>
+          <p className="subline">Template designed by <a href="https://dribbble.com/shots/1817781--FREEBIE-Spirit8-Digital-agency-one-page-template" target="blank" rel="noopener noreferrer">Robert Berki</a> and coded by <a href="https://dribbble.com/jennpereira" target="blank" rel="noopener noreferrer">Jenn Pereira</a></p>
         </div>
         <div className="pull-right fnav">
           <ul className="footer-social">
