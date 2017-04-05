0e769f4062f2edbbb35bada8f4469eced5261294
Mark Hario <hario.mark@gmail.com>
2017-04-04 19:31:16 -0600

    removed owl carousel, fixed team display

--- app/react/components/LandingPage/Team.js
+++ app/react/components/LandingPage/Team.js
@@ -1,24 +1,27 @@
 import React from 'react'
 
+const { Grid, Row, Col } = require('react-flexgrid')
+
 const Team = () => {
   return (
     <div id="tf-team" className="text-center">
       <div className="overlay">
-        <div className="container">
-          <div className="section-title center">
-            <h2>Meet <strong>our team</strong></h2>
-            <div className="line">
-              <hr />
-            </div>
-            <div className="clearfix"></div>
-            <p style={{ 'marginBottom': '20px' }}>
-              <em>We are a remote team of volunteers who are passionate about equality, social justice, and women's rights.</em>
-            </p>
+        <div className="section-title center">
+          <h2>Meet <strong>our team</strong></h2>
+          <div className="line">
+            <hr />
           </div>
+          <div className="clearfix"></div>
+          <p style={{ 'marginBottom': '20px' }}>
+            <em>We are a remote team of volunteers who are passionate about equality, social justice, and women's rights.</em>
+          </p>
+        </div>
 
-          <div id="team" className="owl-carousel owl-theme row">
+        <Grid>
+          <Row>
+
             <div className="item">
-              <div className="thumbnail">
+              <Col className="thumbnail" md={4}>
                 <img src="img/team/rachel.jpg" alt="Rachel Bird" className="img-circle team-img" />
                 <div className="caption">
                   <h3>Rachel Bird</h3>
@@ -26,11 +29,11 @@ const Team = () => {
                   <p>Software developer. Writer. Love warrior.</p> <p>Follow <a href="https://twitter.com/CodingLady" target="blank" rel="noopener noreferrer">@CodingLady</a></p>
                   <p>rachel@workingwomenadvocates.org</p>
                 </div>
-              </div>
+              </Col>
             </div>
 
             <div className="item">
-              <div className="thumbnail">
+              <Col md={4} className="thumbnail">
                 <img src="img/team/beth.jpg" alt="Beth Qiang" className="img-circle team-img" />
                 <div className="caption">
                   <h3>Beth Qiang</h3>
@@ -39,11 +42,11 @@ const Team = () => {
                   <p>Follow <a href="https://twitter.com/BethQiang" target="blank" rel="noopener noreferrer">@BethQiang</a></p>
                   <p>beth@workingwomenadvocates.org</p>
                 </div>
-              </div>
+              </Col>
             </div>
 
             <div className="item">
-              <div className="thumbnail">
+              <Col md={4} className="thumbnail">
                 <img src="img/team/diana.jpg" alt="Diana Arteaga" className="img-circle team-img" />
                 <div className="caption">
                   <h3>Diana Arteaga</h3>
@@ -52,11 +55,11 @@ const Team = () => {
                   <p>Follow <a href="https://twitter.com/_dianart" target="blank" rel="noopener noreferrer">@_dianart</a></p>
                   <p>diana@workingwomenadvocates.org</p>
                 </div>
-              </div>
+              </Col>
             </div>
 
             <div className="item">
-              <div className="thumbnail">
+              <Col md={4} className="thumbnail">
                 <img src="img/team/dennis.jpg" alt="Dennis Sanchez" className="img-circle team-img" />
                 <div className="caption">
                   <h3>Dennis Sanchez</h3>
@@ -65,11 +68,11 @@ const Team = () => {
                   <p>Follow <a href="https://twitter.com/dennycanta" target="blank" rel="noopener noreferrer">@dennycanta</a></p>
                   <p>dennis@workingwomenadvocates.org</p>
                 </div>
-              </div>
+              </Col>
             </div>
 
             <div className="item">
-              <div className="thumbnail">
+              <Col md={4} className="thumbnail">
                 <img src="img/team/mark.png" alt="Mark Hario" className="img-circle team-img" />
                 <div className="caption">
                   <h3>Mark Hario</h3>
@@ -78,11 +81,11 @@ const Team = () => {
                   <p>Follow <a href="https://twitter.com/MarkHario" target="blank" rel="noopener noreferrer">@MarkHario</a></p>
                   <p>mark@workingwomenadvocates.org</p>
                 </div>
-              </div>
+              </Col>
             </div>
 
-          </div>
-        </div>
+          </Row>
+        </Grid>
       </div>
     </div>
   )
